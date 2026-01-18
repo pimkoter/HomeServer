e99249db58b53dd588f94c5b67af700640c36453
pim <pim@NixBTW>
2026-01-18 15:05:27 +0100

    notation oopsie

--- modules/pihole/pihole.nix
+++ modules/pihole/pihole.nix
@@ -1,43 +1,40 @@
 {
-  services.pihole-ftl = {
-    enable = true;
-    lists = [
-      #"example.com"
-      "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/advertising.txt"
-      "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/tracking.txt"
-      "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/malicious.txt"
-      "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/suspicious.txt"
-      "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/comprehensive.txt"
-    ];
-    settings = {
-      dns = {
-        upstreams = ["127.0.0.1#5354"];
-        interface = "ens18";
-        expandHosts = true;
-      };
-      dhcp = {
-        active = true;
-        start = "192.168.178.50";
-        end = "192.168.178.254";
-        router = "192.168.178.1";
-        leaseTime = "12h";
-        #        hosts = [
-        #          "192.168.178.1,router"
-        #          "192.168.178.2,pihole"
-        #          "192.168.178.4,jellyfin"
-        #          #"MAC,IP,NAME"
-        #        ];
-      };
-      webserver = {
-        enable = true;
-        ports = [80];
-        api = {
-          pwhash = "";
+  services = {
+    pihole-ftl = {
+      enable = true;
+      lists = [
+        #"example.com"
+        "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/advertising.txt"
+        "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/tracking.txt"
+        "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/malicious.txt"
+        "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/suspicious.txt"
+        "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/comprehensive.txt"
+      ];
+      settings = {
+        dns = {
+          upstreams = ["127.0.0.1#5354"];
+          interface = "ens18";
+          expandHosts = true;
+        };
+        dhcp = {
+          active = true;
+          start = "192.168.178.50";
+          end = "192.168.178.254";
+          router = "192.168.178.1";
+          leaseTime = "12h";
+          #        hosts = [
+          #          "192.168.178.1,router"
+          #          "192.168.178.2,pihole"
+          #          "192.168.178.4,jellyfin"
+          #          #"MAC,IP,NAME"
+          #        ];
         };
       };
-      misc = {
-        readOnly = false;
-      };
+    };
+
+    pihole-web = {
+      enable = true;
+      port = [80];
     };
   };
 }
