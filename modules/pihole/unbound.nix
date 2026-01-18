27cba683a1359530bf4268186ec732538a109c9a
pim <pim@NixBTW>
2026-01-18 14:31:50 +0100

    fixed it

--- modules/pihole/unbound.nix
+++ modules/pihole/unbound.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: {
   services.unbound = {
     enable = true;
-    interface = "127.0.0.1";
-    port = 5354;
-    accessControl = ["127.0.0.1/32"];
-    rootHints = pkgs.unbound-root-hints;
-    prefetch = true;
     settings = {
+      interface = "127.0.0.1";
+      port = 5354;
+      accessControl = ["127.0.0.1/32"];
+      rootHints = pkgs.unbound-root-hints;
+      prefetch = true;
       msgCacheSize = "16m";
       rrsetCacheSize = "16m";
 
